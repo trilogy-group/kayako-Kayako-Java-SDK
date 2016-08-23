bc02b04090b0d24032dc66f4cb581a7c93eba244
Neil Mathew <mathew.neil@gmail.com>
2016-08-23 15:24:59 +0530

    First Commit

---
+++ src/main/java/com/kayako/sdk/Main.java
@@ -0,0 +1,12 @@
+package com.kayako.sdk;
+
+/**
+ * @author Neil Mathew (neil.mathew@kayako.com)
+ * @date 11/08/16
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("You've reached Kayako. How may we serve you?");
+    }
+}
