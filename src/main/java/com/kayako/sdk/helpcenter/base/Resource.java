cf04ebca674a3aa15025639497d3c4c363011440
Neil Mathew <mathew.neil@gmail.com>
2016-09-12 11:36:15 +0530

    Make Resource serializable

--- src/main/java/com/kayako/sdk/helpcenter/base/Resource.java
+++ src/main/java/com/kayako/sdk/helpcenter/base/Resource.java
@@ -1,8 +1,10 @@
 package com.kayako.sdk.helpcenter.base;
 
+import java.io.Serializable;
+
 /**
  * @author Neil Mathew (neil.mathew@kayako.com)
  * @date 18/08/16
  */
-public interface Resource {
+public interface Resource extends Serializable {
 }
