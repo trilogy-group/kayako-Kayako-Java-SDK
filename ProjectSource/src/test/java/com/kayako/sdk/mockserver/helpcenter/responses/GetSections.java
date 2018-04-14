749984fff544aef886b43e76b45fc032111899bc
rfajardo2008 <rodolfo.fajardo@icloud.com>
2018-04-13 23:15:36 -0500

    201804132315 GetSections extends SampleResponse

--- ProjectSource/src/test/java/com/kayako/sdk/mockserver/helpcenter/responses/GetSections.java
+++ ProjectSource/src/test/java/com/kayako/sdk/mockserver/helpcenter/responses/GetSections.java
@@ -1,30 +1,13 @@
 package com.kayako.sdk.mockserver.helpcenter.responses;
 
-import com.kayako.sdk.mockserver.base.BaseSampleResponse;
+import com.kayako.sdk.mockserver.base.SampleResponse;
 import com.kayako.sdk.utils.MessageLoader;
 
-/**
- * @author Neil Mathew (neil.mathew@kayako.com)
- * @date 15/02/17
- */
-public class GetSections extends BaseSampleResponse {
-    @Override
-    public String getMethod() {
-        return "GET";
-    }
+public class GetSections extends SampleResponse {
 
-    @Override
-    public String getRequestUrl() {
-        return "http://support.kayako.com//api/v1/sections.json?_case=camel&include=localeField,category&limit=10&category_id=1&offset=0";
-    }
-
-    @Override
-    public int getStatusCode() {
-        return 200;
-    }
-
-    @Override
-    public String getBody() {
-        return MessageLoader.readFile("sections", "section", 10);
-    }
+  public GetSections() {
+    super("GET",
+        "http://support.kayako.com//api/v1/sections.json?_case=camel&include=localeField,category&limit=10&category_id=1&offset=0",
+        200, MessageLoader.readFile("sections", "section", 10));
+  }
 }
