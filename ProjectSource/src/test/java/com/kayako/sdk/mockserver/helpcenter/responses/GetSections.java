565640feb4ab3dbd05aac684a0e95ddefe1f8bf2
rfajardo2008 <rodolfo.fajardo@icloud.com>
2018-04-14 19:04:08 -0500

    201804131903 GetSections uses constants

--- ProjectSource/src/test/java/com/kayako/sdk/mockserver/helpcenter/responses/GetSections.java
+++ ProjectSource/src/test/java/com/kayako/sdk/mockserver/helpcenter/responses/GetSections.java
@@ -5,9 +5,17 @@ import com.kayako.sdk.utils.MessageLoader;
 
 public class GetSections extends SampleResponse {
 
+  private static final String METHOD = "GET";
+  private static final String REQUEST_URL =
+      "http://support.kayako.com//api/v1/sections.json?_case=camel&include=localeField,category&limit=10&category_id=1&offset=0";
+  private static final int STATUS_CODE = 200;
+
+  private static final String MESSAGE_FILE = "sections";
+  private static final String CONTENTS_FILE = "section";
+  private static final int CONTENTS_LENGTH = 10;
+
   public GetSections() {
-    super("GET",
-        "http://support.kayako.com//api/v1/sections.json?_case=camel&include=localeField,category&limit=10&category_id=1&offset=0",
-        200, MessageLoader.readFile("sections", "section", 10));
+    super(METHOD, REQUEST_URL, STATUS_CODE,
+        MessageLoader.readFile(MESSAGE_FILE, CONTENTS_FILE, CONTENTS_LENGTH));
   }
 }
