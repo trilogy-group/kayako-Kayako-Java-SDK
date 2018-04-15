377a943275c07d41fedef9d9658e09f2a5d326a3
rfajardo2008 <rodolfo.fajardo@icloud.com>
2018-04-15 00:21:59 -0500

    201804150021 GetConversationList uses constants

--- ProjectSource/src/test/java/com/kayako/sdk/mockserver/messenger/responses/GetConversationList.java
+++ ProjectSource/src/test/java/com/kayako/sdk/mockserver/messenger/responses/GetConversationList.java
@@ -5,9 +5,17 @@ import com.kayako.sdk.utils.MessageLoader;
 
 public final class GetConversationList extends SampleResponse {
 
+  private static final String METHOD = "GET";
+  private static final String REQUEST_URL =
+      "https://kayako-mobile-testing.kayako.com/api/v1/conversations?_case=camel&include=userMinimal,team,caseStatus,casePriority&limit=20&offset=0";
+  private static final int STATUS_CODE = 200;
+
+  private static final String MESSAGE_FILE = "conversation_list";
+  private static final String CONTENTS_FILE = "conversation_list";
+  private static final int CONTENTS_LENGTH = 12;
+
   public GetConversationList() {
-    super("GET",
-        "https://kayako-mobile-testing.kayako.com/api/v1/conversations?_case=camel&include=userMinimal,team,caseStatus,casePriority&limit=20&offset=0",
-        200, MessageLoader.readFile("conversation_list", "conversation_list", 12));
+    super(METHOD, REQUEST_URL, STATUS_CODE,
+        MessageLoader.readFile(MESSAGE_FILE, CONTENTS_FILE, CONTENTS_LENGTH));
   }
 }
